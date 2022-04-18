ced53f5ffc2f67fc76b048f53f88436ca5e6b557
rystew17 <98720917+rystew17@users.noreply.github.com>
2022-04-18 12:46:57 -0400

    Update project.py

--- project.py
+++ project.py
@@ -10,6 +10,9 @@ raw = pd.read_csv("https://raw.githubusercontent.com/rystew17/Project/main/Raw%2
 
 countries = raw['Country\\year']
 
+
+years = pd.DataFrame({[1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019]})
+
 raw = raw.iloc[:,2:]
 estimated = estimated.iloc[:,2:]
 
@@ -19,4 +22,8 @@ estimated = estimated.apply(pd.to_numeric, errors='coerce')
 st.title('CSE 5544 Project')
 
 st.radio("Include esitmated data?", ("Yes", "No"))
+if type == "Yes":
+    option = st.selectbox("Select year", years)
+else:
+    st.write("you selected something else")
 
