fa65676280cf3624c13025cd4726d9720d24c76f
rystew17 <98720917+rystew17@users.noreply.github.com>
2022-04-18 13:04:45 -0400

    Update project.py

--- project.py
+++ project.py
@@ -5,34 +5,137 @@ import seaborn as sns
 import numpy as np
 import altair as alt
 
-estimated = pd.read_csv("https://raw.githubusercontent.com/rystew17/Project/main/Estimated%20Climate%20Data%20Included%20-%20Sheet1.csv")
+"hello cse 5544"
 
-raw = pd.read_csv("https://raw.githubusercontent.com/rystew17/Project/main/Raw%20Climate%20Data%20-%20Sheet1.csv")
+st.title("streamlit quick start")
 
+st.header("Write and magic commands")
 
-years = pd.DataFrame({'c1':[1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019]})
+st.subheader("write subheader")
 
-countries = raw['Country\year']
-raw = raw.iloc[:,1:]
-estimated = estimated.iloc[:,1:]
-raw = raw.apply(pd.to_numeric, errors='coerce')
-estimated = estimated.apply(pd.to_numeric, errors='coerce')
+st.markdown("# h1")
+st.markdown("## h2")
+st.markdown("### h3")
 
+st.latex("\sum_{0}^{n}i")
 
-st.dataframe(raw)
+st.header("Display data")
 
-chart = alt.Chart(raw).mark_bar().encode(
-    x=countries,
-    y='1990'
+st.subheader("Matplotlib chart")
+
+df = pd.DataFrame({
+    'c1':[1,2,3,4],
+    'c2':[10,20,30,40]
+})
+
+data = pd.read_csv("https://raw.githubusercontent.com/CSE5544/data/main/ClimateData.csv")
+data
+
+#prepare the data
+countries = data['Country\\year']
+df_data_country = data.iloc[:,2:]
+df_data_country = df_data_country.apply(pd.to_numeric, errors='coerce')
+country_stats = pd.DataFrame({'country': countries, 'mean': df_data_country.mean(axis=1),
+                       'std': df_data_country.std(axis=1)})
+
+#render results
+fig, ax = plt.subplots(figsize=(14, 6), dpi = 50)
+ax.bar(countries, country_stats['mean'], yerr=country_stats['std'], capsize = 3)
+ax.set_axisbelow(True)  #ensure the grid is under the graph elements
+ax.margins(x=0.01) #set up the margin of graph
+ax.grid(alpha = 0.3) #show the grid line
+ax.set_xlabel('country')
+ax.set_ylabel('emissions')
+ax.set_title('The mean and std of emissions of countries')
+xaxis = plt.xticks(rotation=90, ha='center', fontsize=8)
+yaxis = plt.yticks(fontsize=8)
+
+st.pyplot(fig)
+
+st.subheader("altair chart")
+
+chart_data = data.drop(columns=['Non-OECD Economies'])
+chart_data = pd.melt(chart_data, id_vars=['Country\year'], var_name='year')
+chart_data['value'] = chart_data['value'].apply(pd.to_numeric, errors='coerce')
+chart_data.rename(columns={"Country\year": "country", "value":"emission"}, inplace = True)
+chart_data
+
+#render using altair
+heatmap = alt.Chart(chart_data).mark_rect().encode(
+    x=alt.X('country:N', title = 'country'),
+    y=alt.Y('year:O', title = 'year'),
+    color='emission:Q',
+    tooltip=['country', 'year', 'emission']
 )
 
-st.title('CSE 5544 Project')
-st.altair_chart(chart, use_container_width = True)
+st.altair_chart(heatmap, use_container_width = True)
 
-type = st.radio("Include esitmated data?", ("Yes", "No"))
-if type == "Yes":
-    option = st.selectbox("Select year", years)
-    
+st.header("widgets")
+
+st.subheader("button")
+
+st.button("click me")
+
+if(st.button("about")):
+    st.text("clicked about")
+
+st.subheader("checkbox")
+
+agree = st.checkbox("I agree")
+if agree:
+    st.write("Great, you agreed!")
+
+st.subheader("radio")
+type = st.radio("what's your favorite movie type?", ("Comedy", "Drama", "Action"))
+if type == "Comedy":
+    st.write("You selected Comedy")
 else:
     st.write("you selected something else")
 
+st.subheader("selectbox")
+option = st.selectbox("country", ("USA", "China", "Europe Union"))
+
+st.write(option)
+
+st.subheader("multi-select")
+option = st.multiselect("country", ("USA", "China", "Europe Union"), ("USA"))
+
+st.write(option)
+
+st.subheader("slider")
+x = st.slider("x")
+st.write("the square is", x * x)
+
+st.header("Interactive chart")
+
+st.subheader("interactive matplotlib chart")
+
+options = st.multiselect("select countries", countries, ['Australia'])
+
+country_stats.set_index("country", inplace=True)
+
+
+fig, ax = plt.subplots(figsize=(14, 6), dpi = 50)
+ax.bar(options, country_stats.loc[options]['mean'], yerr=country_stats.loc[options]['std'], capsize = 3)
+ax.set_axisbelow(True)  #ensure the grid is under the graph elements
+ax.margins(x=0.01) #set up the margin of graph
+ax.grid(alpha = 0.3) #show the grid line
+ax.set_xlabel('country')
+ax.set_ylabel('emissions')
+ax.set_title('The mean and std of emissions of countries')
+xaxis = plt.xticks(rotation=90, ha='center', fontsize=8)
+yaxis = plt.yticks(fontsize=8)
+
+st.pyplot(fig)
+
+st.subheader("interactive altair chart")
+
+option = st.selectbox("select one country", countries)
+
+filter_data = chart_data[chart_data['country'] == option]
+bar_chart = alt.Chart(filter_data).mark_bar().encode(
+    x = 'year:O',
+    y = 'emission:Q'
+)
+
+st.altair_chart(bar_chart, use_container_width = True)
