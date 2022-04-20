34315030c0cb90dbc3385d5bff1aca98e2fb650f
rystew17 <98720917+rystew17@users.noreply.github.com>
2022-04-20 17:02:28 -0400

    Update project.py

--- project.py
+++ project.py
@@ -70,8 +70,8 @@ plt.style.use('seaborn')
 #comparison of emission between big five from UN
 
 df_copy1 = df_data
-df_copy1.drop(df_copy1['OECD - Total'], inplace = True)
-df_copy1.drop(df_copy1['OECD - Europe'], inplace = True)
+df_copy1.drop('OECD - Total', inplace = True)
+df_copy1.drop('OECD - Europe', inplace = True)
 df_copy1
 column_names = df_copy1.columns.values
 years = list(column_names)
