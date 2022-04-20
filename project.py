f77514f014097eb23fdf65db5b54224a790f69a5
rystew17 <98720917+rystew17@users.noreply.github.com>
2022-04-20 17:27:57 -0400

    Update project.py

--- project.py
+++ project.py
@@ -7,7 +7,7 @@ import altair as alt
 
 estimated = pd.read_csv("https://raw.githubusercontent.com/rystew17/Project/main/Estimated%20Climate%20Data%20Included%20-%20Sheet1%20(1).csv")
 
-raw = pd.read_csv("https://raw.githubusercontent.com/rystew17/Project/main/Raw%20Climate%20Data%20-%20Sheet1.csv")
+raw = pd.read_csv("https://raw.githubusercontent.com/rystew17/Project/main/Raw%20Climate%20Data%20-%20Sheet1%20(1).csv")
 
 years = pd.DataFrame({'c1':['1990','1991','1992','1993','1994','1995','1996','1997','1998','1999','2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019']})
 
