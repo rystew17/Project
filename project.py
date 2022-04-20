ba8007e1c51694aab395ceb19e0aeff6fa0dca64
rystew17 <98720917+rystew17@users.noreply.github.com>
2022-04-20 17:38:46 -0400

    Update project.py

--- project.py
+++ project.py
@@ -192,9 +192,6 @@ st.pyplot(fig)
 
 df_data2 = pd.read_csv("https://raw.githubusercontent.com/rystew17/Project/main/CSE5544.Lab1.ClimateData%20-%20Sheet1.csv")
 
-# Convert all year columns to float data type
-cols = df_data2.columns.drop(['Country\year', 'Non-OECD Economies'])
-df_data2[cols] = df_data2[cols].apply(pd.to_numeric, errors = 'coerce')
 
 # Drop Non-OECD Economies column
 data = df_data2.drop(columns=['Non-OECD Economies'])
